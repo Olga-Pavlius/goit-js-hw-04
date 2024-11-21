3c2dd70ab5a9ae44ac87545d2a0d172f7e2a8910
Olga Pavlius <olya.melnyk.9412@gmail.com>
2024-11-21 20:41:18 +0200

    add days.length === 0 in task-2

--- js/task-2.js
+++ js/task-2.js
@@ -1,8 +1,9 @@
 function calcAverageCalories(days) {
+  if (days.length === 0) {
+    return 0; 
+  }
    const totalCalories = days.reduce((sum, day) => sum + day.calories, 0);
-  
  return totalCalories / days.length;
-
 }
 
 console.log(
